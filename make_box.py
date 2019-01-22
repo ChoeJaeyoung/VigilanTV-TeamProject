26567ad3854bdbb0227aef966214ce71a4d5ce81
wetPlant <cjy9100@naver.com>
2019-01-22 16:47:25 +0900

    make-box 1-22

--- make_box.py
+++ make_box.py
@@ -2,14 +2,14 @@ import pytesseract
 import cv2
 import PIL
 
-capture_path = "C:/Users/Elite/Desktop/testOCR/plate2/"
+capture_path = "C:/Users/Elite/Desktop/makebox/test/"
 
-for i in range(1,54):
-    img_path = capture_path + "img_(" + str(i) + ").jpg"
-    fp = open(img_path, "rb")
-    img = PIL.Image.open(fp)
-    txt = pytesseract.image_to_boxes(img, lang='kor2')
-    print("ocr 결과 : " + str(i) + txt)
-    f = open('C:/Users/Elite/Desktop/testOCR/img_(' + str(i) + ').box', 'w')
-    f.write(txt)
-    f.close()
+#for i in range(48,57):
+img_path = capture_path + "car" + str(1) + ".jpg"
+fp = open(img_path, "rb")
+img = PIL.Image.open(fp)
+txt = pytesseract.image_to_boxes(img, lang='vkor')
+print("ocr 결과 : " + str(1) + txt)
+f = open('C:/Users/Elite/Desktop/makebox/car' + str(1) + '.box', 'w')
+f.write(txt)
+f.close()
