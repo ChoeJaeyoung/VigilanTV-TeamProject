1806b297a3818e3c090dd55296e35acb6c029b35
jooyoung96 <45277263+jooyoung96@users.noreply.github.com>
2019-01-29 17:01:09 +0900

    음... 만드는중

---
+++ darkflow/IMG_main.py
@@ -0,0 +1,9 @@
+from module import *
+import cv2
+import numpy as np
+import os
+
+carIMG = cv2.imread('car.jpg')
+# identificator = colorIdentification(image1)
+# color = identificator.color()
+# print(color)
