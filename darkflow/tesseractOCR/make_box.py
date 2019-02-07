8672e98a111a05389c1fab1fd2fd694f864da697
wetPlant <cjy9100@naver.com>
2019-02-07 15:55:01 +0900

    2-7 update

--- darkflow/tesseractOCR/make_box.py
+++ darkflow/tesseractOCR/make_box.py
@@ -3,24 +3,25 @@ import pytesseract
 import PIL
 import pandas as pd
 
-capture_path = "C:/Users/Elite/Desktop/makebox/test/"
+capture_path = "C:/Users/Elite/Desktop/1차/"
 all_words = 0
 counts = 0
 fail = []
+han = {}
 
-for i in range(1,5):
+for i in range(1,112):
     #box 불러오기
-    df = pd.read_csv(capture_path + 'car' + str(i) +'.box', sep=' ', header=None)
+    df = pd.read_csv(capture_path + 'test (' + str(i) +').box', sep=' ', header=None)
     box_data = df[0].tolist()
     print(df)
     print(box_data)
 
     #img파일
-    img_path = capture_path + "car" + str(i) + ".jpg"
+    img_path = capture_path + "test (" + str(i) + ").jpg"
     fp = open(img_path, "rb")
     img = PIL.Image.open(fp)
     #config setting
-    config= ('-l 1024all+52font --oem 0 --psm 6')
+    config= ('-l 1499lstm+52font --oem2 --psm6')
     #ocr_result
     txt = pytesseract.image_to_string(img, config= config)
     all_words += 7
@@ -40,10 +41,19 @@ for i in range(1,5):
     f = open(capture_path + 'font_testkor' + '.txt', 'a')
     f.write(txt+ '\n')
 
+    #한글 단어 개수
+    for idx in box_data:
+        if idx in han:
+            han[idx] = han[idx] + 1
+        else:
+            han[idx] = 1
+
 print('총 단어의 수 :' + str(all_words))
 print('틀린 단어 수 : ' + str(counts))
 print('인식율 : ' + str(100 -(counts/all_words*100)))
 for fa in fail:
     print("틀린 번호판 :" + fa)
+
+print('문자별 개수 : ' + str(han))
 
 f.close()
--- darkflow/tesseractOCR/maketrainbox.py
+++ darkflow/tesseractOCR/maketrainbox.py
@@ -3,19 +3,19 @@ import pytesseract
 import PIL
 import pandas as pd
 
-capture_path = "C:/Users/Elite/Desktop/dpi/"
+capture_path = "C:/Users/Elite/Desktop/dpi/up/test/"
 
 
-for i in range(1,391):
+for i in range(323,359):
     #img파일
-    img_path = capture_path + "exam (" + str(i) + ").jpg"
+    img_path = capture_path + "upexam(" + str(i) + ").jpg"
     fp = open(img_path, "rb")
     img = PIL.Image.open(fp)
     #make_box
-    txt = pytesseract.image_to_boxes(img, lang="52font+50kor")
+    txt = pytesseract.image_to_boxes(img, lang="52font+1499")
     print("ocr 결과 : " + txt)
     print('index' + str(i))
-    f = open(capture_path + 'exam ('+ str(i) + ').box', 'w', encoding='utf-8')
+    f = open(capture_path + 'upexam('+ str(i) + ').box', 'w', encoding='utf-8')
     f.write(txt+ '\n')
 
 f.close()
